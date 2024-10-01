9f7c28c3631aac50b17a08ed02100f1ac0f7235c
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-10-02 03:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
 const images = [
-const imageUrls = [
-{ url: "https://picsum.photos/id/237/200/300" },
-{ url: "https://picsum.photos/id/238/200/300" },
-{ url: "https://picsum.photos/id/239/200/300" },
+  {
+    url: "https://picsum.photos/id/237/200/300",
+    alt: "Image 1",
+  },
+  {
+    url: "https://picsum.photos/id/238/200/300",
+    alt: "Image 2",
+  },
+  {
+    url: "https://picsum.photos/id/239/200/300",
+    alt: "Image 3",
+  }
+  
 ];
 
-btn.addEventListener('click', downloadImages);
-
-function downloadImages() {
-    const imagePromises = imageUrls.map(downloadImage);
-
-    Promise.all(imagePromises)
-        .then(images => {
-            const outputDiv = document.getElementById('output');
-            outputDiv.innerHTML = ''; // Clear previous images if any
-            images.forEach(img => outputDiv.appendChild(img));
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
-function downloadImage(image) {
+function downloadImages(images) {
+  const promises = images.map(image => {
     return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = image.url;
-        img.onload = () => resolve(img);
-        img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+      const img = new Image();
+      img.src = image.url;
+      img.alt = image.alt;
+      img.onload = () => {
+        resolve(img);
+      };
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${image.url}`);
+      };
+    });
+  });
+
+  Promise.all(promises)
+    .then(imgs => {
+      const output = document.getElementById('output');
+		output.innerHTML = null;
+      imgs.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
     });
 }
+
+const button = document.getElementById('download-images-button');
+button.addEventListener('click', () => {
+  downloadImages(images);
+});

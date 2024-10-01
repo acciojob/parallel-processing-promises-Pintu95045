528e989fca7a6b1149093eee68ffa0e9871c70eb
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-10-02 02:56:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,22 +8,34 @@ const images = [
 { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-btn.addEventListener("click", () => {
-  const downloadImage = (image) => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = image.url;
-      img.onload = () => resolve(img); 
-      img.onerror = () => reject(new Error(Failed to load image's URL: ${image.url}));
-    });
-  };
+btn.addEventListener("click", downloadAndDisplayImages);
 
+function downloadAndDisplayImages() {
   Promise.all(images.map(downloadImage))
-    .then((loadedImages) => {
-      output.innerHTML = "";
-      loadedImages.forEach((img) => output.appendChild(img));
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
-});
+    .then(displayImages)
+    .catch((error) => console.error(error));
+}
+
+function downloadImage(image) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () =>
+      reject(new Error(`Failed to load image's URL: ${image.url}`));
+    img.src = image.url;
+  });
+}
+
+function displayImages(images) {
+  output.innerHTML = "";
+  images.forEach((img) => {
+    const link = document.createElement("a");
+    link.href = img.src;
+    link.download = true;
+    const imgElement = document.createElement("img");
+    imgElement.src = img.src;
+    imgElement.alt = "Downloaded Image";
+    link.appendChild(imgElement);
+    output.appendChild(link);
+  });
+}
